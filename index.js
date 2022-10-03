f01c73ca81e424b2b4cbcb2f75e918ff43013db5
Khanya <khanya.keswa@gmail.com>
2022-10-02 21:17:08 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+console.log('debug1');
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+// const newHeader = document.getElementsByTagName('newHeader')[0];
+// newHeader.id = "victory";
+newHeader.id = "victory";
+newHeader.textContent = "Khanya is the champion";
+// var newHeader = h1;
+document.body.append(newHeader);
+
+console.log('debug2');
